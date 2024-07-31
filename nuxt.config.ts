ffcd0ff960d3d349e40afa95513d2c985533c0e8
azertypow <nico@villa1203.ch>
2024-07-31 16:10:07 +0200

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
-    app: {
-      baseURL: '/justine.website/',
-    },
+  app: {
+    baseURL: '/justine.website/',
+  },
   css: [
       '@/assets/_main.scss'
   ],
