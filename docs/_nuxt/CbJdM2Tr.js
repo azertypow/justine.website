e594fdafdad6e61f784b64b6b63ca3b0faa638e1
azertypow <nico@villa1203.ch>
2024-08-27 23:47:19 +0200

    build

--- docs/_nuxt/CbJdM2Tr.js
+++
@@ -1,1 +0,0 @@
-const e="https://justine.sdrvl.ch";async function n(){return(await fetch(`${e}/site-info`)).json()}async function o(s){return(await fetch(`${e}/project-by-slug/${s}`)).json()}export{o as a,n as f};
