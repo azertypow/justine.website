e744adb5ca63826de264a5f324af0345ca72650a
azertypow <nico@villa1203.ch>
2024-11-22 11:48:51 +0100

    feat: restore day

--- _utils/formatDateFromString.ts
+++ _utils/formatDateFromString.ts
@@ -2,6 +2,7 @@ export function formatDateFromString(stringDate: string): string {
     const date = new Date (stringDate)
 
     return new Intl.DateTimeFormat('fr-CH', {
+        day: '2-digit',
         month: '2-digit',
         year: 'numeric',
     }).format(date)
