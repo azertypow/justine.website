9eaa9a2c28fd62973b4c2fedda56de89c9e36e98
azertypow <nico@villa1203.ch>
2025-05-05 13:15:53 +0200

    chore: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,15 @@ export default defineNuxtConfig({
   ssr: false,
   app: {
     baseURL: '/',
+      head: {
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            { rel: 'apple-touch-icon', type: 'image/png', href: '/apple-touch-icon.png', sizes: '180x180'},
+            { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32'},
+            { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16'},
+            { rel: 'manifest', href: '/manifest.json'},
+        ]
+      },
   },
   css: [
       '@/assets/_main.scss'
